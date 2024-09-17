11359878253ab62e9d7dad6fe11482e2c43f507f
mksh-su <shishovmike96@gmail.com>
2024-09-17 12:24:33 +0200

    Photoswipe

--- front/js/scripts.js
+++ front/js/scripts.js
@@ -1,5 +1,14 @@
-const hamburger = document.querySelector('.hamburger');
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburger = document.querySelector('.hamburger');
 
-hamburger.addEventListener('click', () => {
-  document.body.classList.toggle('nav-open');
+  hamburger.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+  });
+
+  const lightbox = new PhotoSwipeLightbox({
+    gallery: '#gallery--getting-started',
+    children: 'a',
+    pswpModule: () => import('photoswipe.esm.js')
+  });
+  lightbox.init();
 });
